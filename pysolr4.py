a786591aa0861ebeede423cc98afe2b93a01f639
zeraholladay <zeraholladay@gmail.com>
2013-03-05 21:04:58 -0500

    minor code cleanup.  time for another beer

--- pysolr4.py
+++ pysolr4.py
@@ -11,14 +11,15 @@ class SolrError(Exception):
     pass
 
 class Solr(object):
-    def __init__(self, url='http://localhost:8983/solr'):
+    def __init__(self, url='http://localhost:8983/solr/collection1'):
         self.url = url
 
     def update(self, *docs):
         """
+        Addes a doc. DOES NOT COMMIT!
         """
         json = dumps(docs)
-        url = '%s' % join(self.url, 'update')
+        url = join(self.url, 'update')
         response = requests.post(url,
                                  json,
                                  headers={'Content-type': 'application/json' })
@@ -29,18 +30,20 @@ class Solr(object):
 
     def commit(self):
         """
+        Commits.  Can be chained with update or delete.
         """
         url = join(self.url, 'update')
         response = requests.post(url,
                                  '<commit/>',
                                  headers={'Content-type': 'text/xml' })
         if 200 != response.status_code:
-            raise SolrError('Update failed: %s' % url)
+            raise SolrError('Commit failed: %s' % url)
         else:
             return self
 
     def select(self, *params):
         """
+        Calls the select response handler.
         """
         encoded_params = urlencode([('wt', 'python')] + list(params))
         url = "%s?%s" % ( join(self.url, 'select'), encoded_params )
@@ -52,17 +55,19 @@ class Solr(object):
 
     def get(self, *params):
         """
+        Calls the select response handler.
         """
         encoded_params = urlencode([('wt', 'python')] + list(params))
         url = "%s?%s" % ( join(self.url, 'get'), encoded_params )
         response = requests.get(url)
         if 200 != response.status_code:
-            raise SolrError('Update failed: %s' % url)
+            raise SolrError('Get failed: %s' % url)
         else:
             return eval(response.content)
 
     def delete(self, query):
         """
+        Deletes documents by query.
         """
         url = join(self.url, 'update')
         query = map(str, query)
